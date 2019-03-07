6f0b97a61b74a2cbdd5bc0b8fe378283450c026e
なぶら <ijknabla@gmail.com>
2019-03-07 16:02:20 +0900

    whoamiコマンドを実装。
    サブコマンドのパースができなかった場合、同じ引数でgitを呼び出す。

--- __main__.py
+++ __main__.py
@@ -1,8 +1,48 @@
 
-import argparse
+import sys
 
-from sys import argv
+import argparse
 
 import subprocess as sp
 
-sp.call(['git'] + argv[1:])
+import argparse
+
+
+class InvalidArguments(Exception):
+    pass
+
+from functools import partial
+
+def whoami(args):
+    run = partial(sp.run, universal_newlines=True,
+                  stdout=sp.PIPE, stderr=sp.DEVNULL)
+    name  = run("git config user.name ".split())
+    email = run("git config user.email".split())
+
+    print(f"{name.stdout.strip()} {email.stdout.strip()}")
+
+parser     = argparse.ArgumentParser(prog="mygit")
+subparsers = parser.add_subparsers()
+
+parser_whoami = subparsers.add_parser("whoami")
+parser_whoami.set_defaults(handler=whoami)
+
+try:
+    try:
+        args = parser.parse_args()
+
+        if hasattr(args, 'handler'):
+            args.handler(args)
+        else:
+            # 未知のサブコマンドの場合はヘルプを表示
+            parser.print_help()
+            
+    except SystemExit as ex:
+        returncode, = ex.args
+        if returncode != 0:
+            raise InvalidArguments()
+        raise
+except InvalidArguments:
+    print("call git command")
+    sys.exit(sp.call(["git"] + sys.argv[1:]))
+
